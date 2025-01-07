db99bbab2271bd11a12950dd1ce7fd8368fc8819
Cito2310 <cito2310@gmail.com>
2025-01-07 09:07:36 -0300

    config deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
-# git push -f git@github.com:<USERNAME>/<REPOSITORY NAME>.git main:gh-pages # HERE ADD GITHUB REPOSITORY
+git push -f git@github.com:Cito2310/task-simple-manager.git main:gh-pages # HERE ADD GITHUB REPOSITORY
 
 cd -
